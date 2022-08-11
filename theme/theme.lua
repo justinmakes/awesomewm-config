44a119dd9d28d588719c6f2191252cfa1bbade43
justinr <justin@apsis.co>
2022-08-10 20:56:05 -0700

    initial commit

---
+++ theme/theme.lua
@@ -0,0 +1,5 @@
+-- set using awsetbg command
+    -- theme.wallpaper_cmd = { "awsetbg -f .config/awesome/themes/awesome-wallpaper.png" }
+
+-- OR set using wallpaper property
+    theme.wallpaper = ".config/awesome/themes/awesome-wallpaper.png"
